69dd6ee2d251789a679e50ed09bd751ba1f333b8
NATHANIEL CARLOS <Carlosnathaniel2024@gmail.com>
2025-08-19 12:25:07 +0800

    Added a user designation, time and updated at details to tweet

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 # Create your models here.
 # Models are used as a Database where we can create new Tables, Add Fields and Add
@@ -11,10 +12,19 @@ from django.db import models
 # Also, make migrations after dealing with Models
 # After creating a superuser, go Import your model to the Admin.py
 class Tweet(models.Model):
-    content = models.TextField()
+    #content = models.TextField()
+# v The use of ForeignKey is to assign a tweet to a user
+#   CASCADE deletes any content related to a deleted object or user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+# v Expanding Tweet to CharField to give flexibility to assign max characters
+    content = models.CharField(max_length=500)
+# v Adding additional Tracking for when a tweet is updated and created
+    updated = models.DateTimeField(auto_now=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
 # v Updating model to fit user-friendly value
 # v Creating Function inside the model
     def __str__(self):
         return str(self.content)
     # ^ This block assigns a string value for each object.
     # for now the title of the tweet would be the tweet itself
+
